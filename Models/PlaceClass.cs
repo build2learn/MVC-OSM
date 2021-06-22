88c7e74cf35a96b82f529d16fe7e41f201db0316
Abdelwahed Abdelmageed <abdelwahed311@hotmail.com>
2021-06-22 19:55:18 +0200

    Display Name Commit

--- Models/PlaceClass.cs
+++ Models/PlaceClass.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
@@ -9,12 +10,18 @@ namespace MVC_OSM.Models
 {
     [Table("place", Schema = "public")]
 
+    //[DisplayName("Places")]
     public class PlaceClass
     {
         [Key]
         public int placeid { get; set; }
+        [DisplayName("Name")]
         public string placname { get; set; }
+
+        [DisplayName("Place Type")]
         public string plactype { get; set; }
+
+        [DisplayName("Address")]
         public string placaddress { get; set; }
         //public DateTime created_on { get; set; }
     }
